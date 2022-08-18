a6615366d8f69f67095d5f8c81bee00a34aaa8d1
harshedabdulla <harshed2004@gmail.com>
2022-08-18 15:00:16 +0530

    heroku changes

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static("public"));
 
-app.listen(3000,function(){
+app.listen(process.env.PORT || 3000,function(){
     console.log("Server is running");
 })
 
