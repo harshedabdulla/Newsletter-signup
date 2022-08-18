41a978d012b3c7edb2a93ece03520e6e968d0e26
harshedabdulla <harshed2004@gmail.com>
2022-08-18 11:29:50 +0530

    made some changes

--- app.js
+++ app.js
@@ -7,10 +7,20 @@ const request=require("request");
 const app=express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(express.static("public"));
+
 app.listen(3000,function(){
     console.log("Server is running");
 })
 
 app.get("/",function(req,res){
-    res.send("Server is up and running");
+    res.sendFile(__dirname+"/signup.html");
 })
+
+app.post("/",function(req,res){
+
+    var firstname = req.body.fName;
+    var lastname = req.body.lName;
+    var email = req.body.email;
+    console.log(firstname,lastname,email);
+});
